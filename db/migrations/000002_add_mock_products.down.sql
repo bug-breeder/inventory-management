3e2837be090b170ad26eea8495d4146dec0541ef
Anh Nguyen <anh.ntv@teko.vn>
2024-05-17 23:44:02 +0700

    delete all rows in the table instead of mock data only

--- db/migrations/000002_add_mock_products.down.sql
+++ db/migrations/000002_add_mock_products.down.sql
@@ -1,5 +1,7 @@
 -- Delete products
-DELETE FROM products WHERE product_name IN ('Laptop Acer Swift 3', 'Pc Phong Vũ 1', 'Aó khoác nam', 'Sổ tay Teko');
+DELETE FROM products;
+-- WHERE product_name IN ('Laptop Acer Swift 3', 'Pc Phong Vũ 1', 'Aó khoác nam', 'Sổ tay Teko');
 
 -- Delete categories
-DELETE FROM product_categories WHERE id IN (1, 2, 3);
+DELETE FROM product_categories;
+--  WHERE id IN (1, 2, 3);
